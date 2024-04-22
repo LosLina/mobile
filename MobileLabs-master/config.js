4c9e8a86a45d07f9a77fe37ed61c81f12d1cf595
LosLina <101467225+LosLina@users.noreply.github.com>
2024-04-22 14:49:07 +0300

    Update config.js

--- MobileLabs-master/config.js
+++ MobileLabs-master/config.js
@@ -1,6 +1,6 @@
 const Config = {
-    newsUrl: '',
-    galleryUrl: '',
+    newsUrl: 'https://raw.githubusercontent.com/LosLina/mobile/main/MobileLabs-master/data/news.json?token=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',
+    galleryUrl: 'https://raw.githubusercontent.com/LosLina/mobile/main/MobileLabs-master/data/images.json?token=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',
 };
 
 export default Config;
